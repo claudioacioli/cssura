ae81ffd469c1df1b6ec4ce54e8a0ae7a4d1b3bd8
Claudio Acioli <claudio.acioli.silva@gmail.com>
2020-09-09 19:32:08 -0300

    chore: Add plugins to optimize

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,30 @@
 const path = require('path')
 const webpack = require('webpack')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
 
 module.exports = {
   entry: './src/index.js',
   mode: 'production',
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({}),
+      new OptimizeCSSAssetsPlugin({})
+    ]
+  },
   module: {
     rules: [
       {
         test: /\.scss$/,
-        use: ['css-loader','sass-loader']
+        use: [MiniCssExtractPlugin.loader, 'css-loader','sass-loader']
       }
     ]
-  }
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'cssura-[name].css'
+    })
+  ]
 }
